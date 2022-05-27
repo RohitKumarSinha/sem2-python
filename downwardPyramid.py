f0f9dfe5dab5a80fe711e75f010163640cd20868
Rohit Kumar Sinha <48133845+RohitKumarSinha@users.noreply.github.com>
2022-05-27 10:45:02 +0530

    Add files via upload

---
+++ downwardPyramid.py
@@ -0,0 +1,10 @@
+def downwardPyramid():
+    num = eval(input("Enter the value of n :- "))
+    for i in range(num, 0, -1):
+        for j in range(0, i):
+            print("* " ,end="")
+        print("\r")
+
+
+downwardPyramid()
+
