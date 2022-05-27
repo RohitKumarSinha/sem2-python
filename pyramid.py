7638df93df9e0f0a78a026bc3b24f57abef6b8d5
Rohit Kumar Sinha <48133845+RohitKumarSinha@users.noreply.github.com>
2022-05-27 10:38:22 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,10 @@
+def pyramid():
+    num = eval(input("Enter the value of n :- "))
+    for i in range(0, num):
+        for j in range(0, i+1):
+            print("* " ,end="")
+        print("\r")
+
+
+pyramid()
+
