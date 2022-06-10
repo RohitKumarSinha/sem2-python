5dab10b7e26b2560e5a7832f3469e48e3775e9b2
Rohit Kumar Sinha <48133845+RohitKumarSinha@users.noreply.github.com>
2022-06-10 11:26:35 +0530

    Add files via upload

---
+++ powerOfTwo.py
@@ -0,0 +1,8 @@
+def powerOfTwo():
+    num = int(input("Enter the number :- "))
+    result = list(map(lambda x: 2 ** x, range(num + 1)))
+    for i in range(num + 1):
+        print("2 raised to power",i,"is",result[i])
+
+
+powerOfTwo()
---
+++ swap.py
@@ -0,0 +1,11 @@
+def swap():
+    num1 = eval(input("Enter first number:- "))
+    num2 = eval(input("Enter second number:- "))
+
+    temp = num1
+    num1 = num2
+    num2 = temp
+
+    print("Your first and second number are :- ", num1, num2)
+
+swap()
